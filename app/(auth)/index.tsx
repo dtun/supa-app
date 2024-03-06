2090cedb3927c0cbda435d0c91a9e890bfa56e97
dtun <dtun@me.com>
2024-03-06 06:20:27 -0700

    Add todo functionality

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,9 +1,57 @@
-import { Text, View } from 'react-native';
+import { supabase } from '@/utils/supabase';
+import { useState } from 'react';
+import { Button, TextInput, View } from 'react-native';
 
 const Page = () => {
+  const [todo, setTodo] = useState('');
+  const [loading, setLoading] = useState(false);
+  const addTodo = async () => {
+    setLoading(true);
+
+    const {
+      data: { user: User },
+    } = await supabase.auth.getUser();
+    const newTodo = {
+      user_id: User?.id,
+      task: todo,
+    };
+
+    await supabase.from('todos').insert(newTodo).single();
+
+    setTodo('');
+    setLoading(false);
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          gap: 10,
+          backgroundColor: '#151515',
+          padding: 6,
+        }}
+      >
+        <TextInput
+          value={todo}
+          onChangeText={setTodo}
+          style={{
+            flex: 1,
+            backgroundColor: '#363636',
+            color: '#fff',
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#2b825b',
+            borderRadius: 4,
+          }}
+        />
+        <Button
+          onPress={addTodo}
+          title="Add"
+          color="#2b825b"
+          disabled={loading || todo === ''}
+        />
+      </View>
     </View>
   );
 };
