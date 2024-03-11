9b1b5145730f89f6a06c958cdf0b3fca6ff36279
dtun <dtun@me.com>
2024-03-11 07:33:02 -0700

    Add image picker

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,11 +1,41 @@
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { View, Image, StyleSheet, Button } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 const Profile = () => {
+  const [image, setImage] = useState<string | null>();
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) setImage(result.assets[0].uri);
+  };
+
   return (
     <View>
-      <Text>Profile</Text>
+      {image ? (
+        <Image source={{ uri: image }} style={styles.avatar} />
+      ) : (
+        <View style={styles.avatar} />
+      )}
+      <Button title="Set Avatar" onPress={pickImage} />
     </View>
   );
 };
 
 export default Profile;
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#ccc',
+    alignSelf: 'center',
+    borderRadius: 100,
+    margin: 40,
+  },
+});
