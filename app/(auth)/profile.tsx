fb203f6946036c2fddb395aba333a59ceb782142
dtun <dtun@me.com>
2024-03-12 07:53:23 -0700

    Add image upload and display functionality

--- app/(auth)/profile.tsx
+++ app/(auth)/profile.tsx
@@ -1,6 +1,9 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { View, Image, StyleSheet, Button } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { supabase } from '@/utils/supabase';
+import * as FileSystem from 'expo-file-system';
+import { decode } from 'base64-arraybuffer';
 
 const Profile = () => {
   const [image, setImage] = useState<string | null>();
@@ -12,8 +15,44 @@ const Profile = () => {
       quality: 1,
     });
 
-    if (!result.canceled) setImage(result.assets[0].uri);
+    if (result.canceled) return;
+
+    setImage(result.assets[0].uri);
+
+    const {
+      data: { user: User },
+    } = await supabase.auth.getUser();
+    const img = result.assets[0];
+    const base64 = await FileSystem.readAsStringAsync(img.uri, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    const filePath = `${User?.id}/avatar.png`;
+    const contentType = 'img/png';
+
+    await supabase.storage
+      .from('avatars')
+      .upload(filePath, decode(base64), { contentType });
   };
+
+  const loadUserAvatar = async () => {
+    const {
+      data: { user: User },
+    } = await supabase.auth.getUser();
+
+    supabase.storage
+      .from('avatars')
+      .download(`${User?.id}/avatar.png`)
+      .then(({ data }) => {
+        if (!data) return;
+        const url = URL.createObjectURL(data);
+
+        setImage(url);
+      });
+  };
+
+  useEffect(() => {
+    loadUserAvatar();
+  }, []);
 
   return (
     <View>
